1506874072f396dec4d4f07bca89e3d182bc14d8
Erlend Hamberg <erlend@hamberg.no>
2017-02-22 15:36:54 +0100

    First working version

---
+++ Karabiner Profile Switcher/ProfileSwitcher.swift
@@ -0,0 +1,58 @@
+//
+//  ProfileSwitcher.swift
+//  Karabiner Profile Switcher
+//
+//  Created by Erlend Hamberg on 22/02/2017.
+//  Copyright © 2017 Erlend Hamberg. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+func enableProfile(profileName: String) {
+    let configFile = FileManager.default
+        .homeDirectoryForCurrentUser
+        .appendingPathComponent(".config/karabiner/karabiner.json")
+
+
+    guard let config = try? String(contentsOf: configFile, encoding: .utf8) else {
+        fatalError("Error when reading config file \(configFile)")
+    }
+
+    var json = JSON(parseJSON: config)
+
+    var idx = 0
+    var updated = 0
+    for (_, var profileJson) in json["profiles"] {
+        guard let name = profileJson["name"].string else {
+            fatalError("Error while reading profiles from config")
+        }
+
+        // if the name matches, set it as the selected profile, otherwise deselect it
+        if name == profileName {
+            profileJson["selected"].bool = true
+            json["profiles"][idx] = profileJson
+
+            updated += 1
+        } else {
+            profileJson["selected"].bool = false
+            json["profiles"][idx] = profileJson
+        }
+
+        idx += 1
+    }
+
+    switch updated {
+    case 0:
+        fatalError("Profile “\(profileName)” not found")
+    case 1:
+        do {
+            try json.description.write(to: configFile, atomically: true, encoding: .utf8)
+            print("Wrote updated karabiner config.")
+        } catch {
+            fatalError("Error when writing config file: \(error)")
+        }
+    default:
+        fatalError("Found more than one profile matching “\(profileName)” o_O")
+    }
+}
--- Karabiner Profile Switcher/main.swift
+++ Karabiner Profile Switcher/main.swift
@@ -8,5 +8,31 @@
 
 import Foundation
 
-print("Hello, World!")
+let defaultProfile = "Built-in keyboard"
+let deviceProfile = "Kinesis Advantage"
+let vendorId = 0x05f3
+let productId = 0x0081
 
+let keyboardDetector = IOUSBDetector(vendorID: vendorId, productID: productId)
+keyboardDetector?.callbackQueue = DispatchQueue.global()
+keyboardDetector?.callback = {
+    (detector, event, service) in
+    switch event {
+    case .Matched:
+        print("Connected. Enabling device profile (\(deviceProfile))")
+        enableProfile(profileName: deviceProfile)
+    case .Terminated:
+        print("Connected. Enabling default profile (\(defaultProfile))")
+        enableProfile(profileName: defaultProfile)
+    }
+};
+
+_ = keyboardDetector?.startDetection()
+
+signal(2, {
+    signal in
+    keyboardDetector?.stopDetection()
+    exit(0)
+})
+
+while true { sleep(1) }
