1552c8fc95cfff8e961d8857ae1c42ae58945fbe
Erlend Hamberg <erlend@hamberg.no>
2017-02-22 15:30:59 +0100

    Initialize swift-package

---
+++ Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+    name: "Karabiner Profile Switcher",
+    dependencies: [
+        .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", versions: Version(1,0,0)..<Version(3, .max, .max)),
+    ]
+    )
