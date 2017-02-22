7324c197f94ea3ac3510d9a1d02fd02268a98379
Erlend Hamberg <erlend@hamberg.no>
2017-02-22 10:43:19 +0100

    Initial commit

---
+++ Karabiner Profile Switcher/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Karabiner Profile Switcher
+//
+//  Created by Erlend Hamberg on 22/02/2017.
+//  Copyright © 2017 Erlend Hamberg. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
