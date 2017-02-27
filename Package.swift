2718ae7ba7e2c154c2f3b662db01efbc573e47bf
Erlend Hamberg <erlend@hamberg.no>
2017-02-27 19:47:29 +0100

    Update dependencies' version bounds

--- Package.swift
+++ Package.swift
@@ -5,11 +5,11 @@ let package = Package(
     dependencies: [
         .Package(
             url: "https://github.com/SwiftyJSON/SwiftyJSON.git",
-            versions: Version(1, 0, 0)..<Version(3, .max, .max)
+            versions: Version(3, 1, 0)..<Version(3, .max, .max)
         ),
         .Package(
             url: "https://github.com/jatoben/CommandLine.git",
-            versions: Version(3, 0, 0)..<Version(4, .max, .max)
+            versions: Version(2, 0, 0)..<Version(3, .max, .max)
         ),
         ]
     )
